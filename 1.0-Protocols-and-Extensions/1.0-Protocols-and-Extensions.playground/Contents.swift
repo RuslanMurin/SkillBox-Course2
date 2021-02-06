909ac8dce5b1151de5297877e8bd6d0c043cd435
Ruslan <Murin>
2021-02-06 19:17:09 +0300

    1.1
    Доброго времени суток, Дмитрий
    Сгладил углы, шахматы делегируют события

--- 1.0-Protocols-and-Extensions/1.0-Protocols-and-Extensions.playground/Contents.swift
+++ 1.0-Protocols-and-Extensions/1.0-Protocols-and-Extensions.playground/Contents.swift
@@ -3,25 +3,84 @@ import UIKit
 
 /*4. Напишите, в чём отличие класса от протокола.
  //Класс описывает свойства и методы, протокол же их только обозначает. Соответсвенно класс - самостоятельный тип, который можно присвоить объекту, протокол же - нет.
+ UPD:
+ protocol A {}
+ class AImpl: A {}
+
+ let a: A = AImpl()
+ 
+ Да, константа а является типом протокола, однако мы можем создать class BImpl: A {} и его экзепляу тоже можно дать тип А.
+ Если выражаться точнее, то класс - это "Конечная" реализация, а протокол лишь "макет". Он только задает логику, но не описывает ее.(Да, логику можно описать в extension, но она будет всего лишь дефолтной, её можно переопределить)
+ Соответсвенно, все вышесказанное дает нам возможность создать некий протокол A с определенным набором поведения, который
+ будут принимать в себя классы AImpl и BImpl. Они будут вынуждены описывать одинаковые методы из протокола А, но вот делать они это могут совершенно по-разному. Соответственно объекты подписанные под классами AImpl/BImpl могут быть типа A.
  
  5. Ответьте, могут ли реализовывать несколько протоколов:
  a) классы (Class),
  b) структуры (Struct)
  c) перечисления (Enum)
  d) кортежи (Tuples).
- Протоколы поддерживают множественное наследование, но только для классов, структур и перечислений
+ Протоколы поддерживают множественное наследование, но только для классов, структур и перечислений. кортежи вообще не могут принять протокол7
  
  6. Создайте протоколы для:
- a) Игры в шахматы против компьютера: 1) протокол-делегат с функцией, через которую шахматный движок будет сообщать о ходе компьютера (с какой на какую клетку); 2) протокол для шахматного движка, в который передаётся ход фигуры игрока (с какой на какую клетку); Double-свойство, возвращающее текущую оценку позиции (без возможности изменения этого свойства) и свойство делегата.*/
-protocol ChessBoardDelegate{
-    func computerDidMove() -> (x: Int, y: Int) //возвращаем ход
+ a) Игры в шахматы против компьютера: 1) протокол-делегат с функцией, через которую шахматный движок будет сообщать о ходе компьютера (с какой на какую клетку); 2) протокол для шахматного движка, в который передаётся ход фигуры игрока (с какой на какую клетку); Double-свойство, возвращающее текущую оценку позиции (без возможности изменения этого свойства) и свойство делегата.
+ */
+enum CellAlphabetic: String{
+    case a = "A"
+    case b = "B"
+    case c = "C"
+    case d = "D"
+    case e = "E"
+    case f = "F"
+    case g = "G"
+    case h = "H"
 }
-protocol ChessEngine{
-    var delegate: ChessBoardDelegate { get }  //делегат
-    var estimation: Double { get }            //оценка
-    func calculateMove(x: Int, y: Int)        //вычисление хода на основании хода игрока
+enum CellNumberic: Int{
+    case one = 1
+    case two = 2
+    case three = 3
+    case four = 4
+    case five = 5
+    case six = 6
+    case seven = 7
+    case eight = 8
+}
+typealias ChessCoordinates = (alpha: CellAlphabetic, number: CellNumberic)
+
+protocol ChessBoardDelegate{
+    var computerDelegate: ChessEngine? { get set }
+    func computerDidMove(at: ChessCoordinates)//возвращаем ход
+}
+extension ChessBoardDelegate{
+    func computerDidMove(at: ChessCoordinates){
+        print(at.alpha.rawValue, at.number.rawValue)
+    }
 }
 
+protocol ChessEngine{
+    var delegate: ChessBoardDelegate? { get set }  //делегат
+    var estimation: ChessCoordinates { get }            //оценка
+    func calculateMove(coordinates: ChessCoordinates)        //вычисление хода на основании хода игрока
+}
+extension ChessEngine{
+    func calculateMove(coordinates: ChessCoordinates) {
+        delegate?.computerDidMove(at: estimation)
+    }
+}
+class ComputerChess: ChessEngine{
+    var delegate: ChessBoardDelegate?
+    var estimation = ChessCoordinates(CellAlphabetic.a, CellNumberic.one)
+}
+class PlayerChess: ChessBoardDelegate{
+    var computerDelegate: ChessEngine?
+    func playerDidMove(at: ChessCoordinates){
+        computerDelegate?.calculateMove(coordinates: at)
+    }
+}
+var player = PlayerChess()
+var computer = ComputerChess()
+computer.delegate = player
+player.computerDelegate = computer
+player.playerDidMove(at: ChessCoordinates(CellAlphabetic.b, CellNumberic.six))
 // b) Компьютерной игры: один протокол для всех, кто может летать (Flyable), второй — для тех, кого можно отрисовывать в приложении (Drawable). Напишите класс, который реализует эти два протокола.
 
 protocol Flyable{
@@ -47,22 +106,24 @@ ultimateBoss.canBeDrawed()
 //  a) CGRect, которая возвращает CGPoint с центром этого CGRect’а.
 extension CGRect{
     func returnCenter()->CGPoint{
-        return CGPoint(x: self.midX, y: self.midY)
+        return CGPoint(x: midX, y: midY)
     }
+    var square: Double { return Double(width * height) }
     func returnSquare()->Double{
-        return Double(self.width * self.height)
+        return Double(width * height)
     }
 }
 let rect = CGRect(x: 0, y: 0, width: 50, height: 50)
+rect.square
 rect.returnCenter()
 // b) CGRect, которая возвращает площадь этого CGRect’а.
 rect.returnSquare()
 // c) UIView, которое анимированно её скрывает (делает alpha = 0).
 extension UIView{
     func hide(){
-        UIView.animate(withDuration: 2.0, animations: {
+        UIView.animate(withDuration: 2.0){
             self.alpha = 0
-        })
+        }
     }
 }
 let view = UIView()
@@ -71,7 +132,8 @@ view.hide()
 print(view.alpha)
 // d) Протокола Comparable, который на вход получает ещё два параметра того же типа: первый  ограничивает минимальное значение, второй —  максимальное, — и возвращает текущее значение, ограниченное этими двумя параметрами.
 extension Comparable{
-    func bound(minX: Self, maxX: Self) -> Self {
+    func bound(minX: Self, maxX: Self) -> Self? {
+        guard minX < maxX else { return nil }
         switch self {
         case ...minX:
             return minX
@@ -83,7 +145,7 @@ extension Comparable{
     }
 }
 var three = 3
-three.bound(minX: 1, maxX: 5)
+three.bound(minX: 100, maxX: 5)
 
 //e) Array, который содержит элементы типа Int: функцию для подсчёта суммы всех элементов.
 extension Array where Element == Int{
