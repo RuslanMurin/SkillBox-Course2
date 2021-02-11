0e59b9dcd13bc5dccc9e08bb745b1957302c9977
Ruslan <Murin>
2021-02-11 19:31:35 +0300

    2.0.1

--- 2.0-Generics/2.0-Generics.playground/Contents.swift
+++ 2.0-Generics/2.0-Generics.playground/Contents.swift
@@ -10,17 +10,17 @@ import UIKit
  */
 // 6. Создайте функцию, которая:
 //a) получает на вход два Equatable-объекта и, в зависимости от того, равны ли они друг другу, выводит разные сообщения в лог;
-func equalsInLog<T: Equatable>(x: T, y: T){
+func equalsInLog<T: Equatable>(x: T, y: T) {
     x == y ? print("elements is equal") : print("elements is not equal")
 }
 equalsInLog(x: 5, y: 5)
 //b) получает на вход два сравниваемых (Comparable) друг с другом значения, сравнивает их и выводит в лог наибольшее;
-func compareInLog<T: Comparable>(x: T, y: T){
+func compareInLog<T: Comparable>(x: T, y: T) {
     x > y ? print(x) : print(y)
 }
 compareInLog(x: 2, y: 5)
 //c) получает на вход два сравниваемых (Comparable) друг с другом значения, сравнивает их и перезаписывает первый входной параметр меньшим из двух значений, а второй параметр — большим;
-func swapForMe<T: Comparable>(_ x: inout T, _ y: inout T){
+func swapForMe<T: Comparable>(_ x: inout T, _ y: inout T) {
     if x > y{
         swap(&x, &y)
     }
@@ -30,23 +30,24 @@ var greater = 3
 swapForMe(&less, &greater)
 print(less, greater)
 //d) получает на вход две функции, которые имеют дженерик — входной параметр Т и ничего не возвращают; сама функция должна вернуть новую функцию, которая на вход получает параметр с типом Т и при своём вызове вызывает две функции и передаёт в них полученное значение (по факту объединяет две функции).
-//func closures<T>(first:(T)->Void, second:(T)->Void)->((T)->Void){
-//    return {_ in first()
-//        second()
-//    }
-//} немного не понял, каким должно быть тело функции
+func closures<T>(first: @escaping (T)->Void, second: @escaping (T)->Void)->((T)->Void) {
+    return {parameter in
+        first(parameter)
+        second(parameter)
+    }
+}
 // 7. Создайте расширение для:
 //a) Array, у которого элементы имеют тип Comparable, и добавьте генерируемое свойство, которое возвращает максимальный элемент;
 extension Array where Element: Comparable{
-    var maxEl: Element? { return self.max() }
+    var maxEl: Element? { return self.sorted(by: >).first }
 }
 let max = [5, 3, 18, 34, 200, 1].maxEl
 //b) Array, у которого элементы имеют тип Equatable, и добавьте функцию, которая удаляет из массива идентичные элементы.
 extension Array where Element: Equatable{
-    mutating func removeDuplicates(){
-        var temp = Array()
+    mutating func removeDuplicates() {
+        var temp: [Element] = []
         for el in self{
-            if temp.contains(el) == false{
+            if !temp.contains(el){
                 temp.append(el)
             }
         }
@@ -55,23 +56,41 @@ extension Array where Element: Equatable{
 }
 var array = [5, 6, 7, 3, 6, 33, 6, 18, 15, 5]
 array.removeDuplicates()
+extension Array where Element: Hashable {
+    mutating func removeDuplicates() {
+        var buffer: [Element] = []
+        var added = Set<Element>()
+        for elem in self {
+            if !added.contains(elem) {
+                buffer.append(elem)
+                added.insert(elem)
+            }
+        }
+        self = buffer
+    }
+}//Так еще можно реализовать
 //8. Создайте специальный оператор для:
 //a) возведения Int-числа в степень: оператор ^^, работает 2^3, возвращает 8;
 postfix operator ^^
-postfix func ^^(number: Int) -> Int{
+postfix func ^^(number: Int) -> Int {
     return number * number * number
 }
 var two = 2^^
+infix operator ^^
+func ^^(left: Int, right: Int) -> Int {
+    return Int(pow(Double(left), Double(right)))
+}
+5 ^^ 3
 //b) копирования во второе Int-числа удвоенного значения первого 4 ~> a, после этого a = 8;
 infix operator ~>
-func ~>(lh: Int, rh: inout Int){
+func ~>(lh: Int, rh: inout Int) {
     rh = lh * 2
 }
-var a = Int()
+var a = 0
 4 ~> a
 //c) присваивания в экземпляр tableView делегата: myController <* tableView, поле этого myController становится делегатом для tableView;
 infix operator <*
-func <*<T: UITableViewDelegate>(delegate: T, tableView: UITableView){
+func <*(delegate: UITableViewDelegate, tableView: UITableView) {
     tableView.delegate = delegate
 }
 class MyVC: UIViewController, UITableViewDelegate{
@@ -80,7 +99,7 @@ let vc = MyVC()
 let table = UITableView()
 vc <* table
 //d) суммирует описание двух объектов с типом CustomStringConvertable и возвращает их описание: 7 + “ string” вернет “7 string”.
-func +(left:CustomStringConvertible, right: CustomStringConvertible)->String{
+func +(left:CustomStringConvertible, right: CustomStringConvertible)->String {
     return left.description + right.description
 }
 let seven = 7
@@ -99,7 +118,7 @@ protocol Animator{
 }
 
 infix operator -*
-func -*<T: Animator>(left: T, right: T.Target){
+func -*<T: Animator>(left: T, right: T.Target) {
     left.animate(right)
 }
 
@@ -110,7 +129,7 @@ class BackGroundAnimator: Animator{
         newValue = value
     }
     func animate(_ target: UIView) {
-        UIView.animate(withDuration: 1.0){ [self] in
+        UIView.animate(withDuration: 1.0) { [self] in
             target.backgroundColor = newValue
         }
     }
@@ -122,7 +141,7 @@ class CenterAnimator: Animator{
         newValue = value
     }
     func animate(_ target: UIView) {
-        UIView.animate(withDuration: 1.0){ [self] in
+        UIView.animate(withDuration: 1.0) { [self] in
             target.center = CGPoint(x: newValue.0, y: newValue.1)
         }
     }
@@ -134,7 +153,7 @@ class ScaleAnimator: Animator{
         newValue = value
     }
     func animate(_ target: UIView) {
-        UIView.animate(withDuration: 1.0){ [self] in
+        UIView.animate(withDuration: 1.0) { [self] in
             target.transform = CGAffineTransform(scaleX: newValue, y: newValue)
         }
     }
